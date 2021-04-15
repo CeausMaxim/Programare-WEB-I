d0d1fb3f53e1540a6a52615519289a4df2e93ab7
CeausMaxim <55556332+CeausMaxim@users.noreply.github.com>
2021-04-15 13:55:45 +0300

    FINAL

--- Vue mini test 15.04.2021/js/app.js
+++ Vue mini test 15.04.2021/js/app.js
@@ -1,5 +1,5 @@
 // EX 1 and EX 2
-const app = new Vue({
+/* const app = new Vue({
     el: '#application',
     data: {
         username: 'Ceaus Maxim!',
@@ -10,20 +10,58 @@ const app = new Vue({
             return new Date().getFullYear() - new Date(this.year).getFullYear();
         }
     }
-})
+}) */
 
 // EX 3
-/* var app = new Vue({
+const app = new Vue({
     el: '#app',
     data: {
-        JS: 1,
-        Python: 0.7,
-        Java: 1.3,
-        Exp: ''
+        salary: 0,
+        lang: "",
+        exp: ""
     },
-    computed: {
-        salaryJs() {
-            return Exp * this.JS
+    watch: {
+        lang() {
+            this.salary = this.salaryProg(this.lang, this.exp);
+        },
+        exp() {
+            this.salary = this.salaryProg(this.lang, this.exp);
+        }
+    },
+    methods: {
+        salaryProg(lang, exp) {
+            let coef = 0;
+            switch (lang) {
+                case "JavaScript":
+                    coef = 1;
+                    break;
+                case "Python":
+                    coef = 0.7;
+                    break;
+                case "Java":
+                    coef = 1.3;
+                    break;
+            }
+
+            let BaseRate = 0;
+            switch (exp) {
+                case "1":
+                    BaseRate = 10000;
+                    break;
+                case "2":
+                    BaseRate = 12000;
+                    break;
+                case "3":
+                    BaseRate = 13500;
+                    break;
+                case "4":
+                    BaseRate = 14000;
+                    break;
+                case "5":
+                    BaseRate = 15000;
+                    break;
+            }
+            return BaseRate * coef;
         }
     }
-}) */
+})
